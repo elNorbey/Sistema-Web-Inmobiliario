5195818fe3f096209cbcc2cfbc4f0125cc5e61a8
Norbey <norbeycastillo890@gmail.com>
2026-07-03 22:06:22 -0600

    Changes from/ Desarrollo

---
+++ desarrollo/public/index.php
@@ -0,0 +1,45 @@
+<?php
+/*
+======================================================
+        SISTEMA WEB INMOBILIARIO CJINVERSIONES
+======================================================
+
+Archivo:
+index.php
+
+Descripcion:
+Punto de entrada principal del sitio web.
+Desde aqui se carga toda la aplicacion.
+
+Desarrollador:
+Ing. Norbey Castillo
+
+Version:
+0.1
+
+*/
+
+declare(strict_types = 1);
+?>
+
+<!DOCTYPE html>
+
+<html lang = "es">
+
+<head>
+    <meta charset = "UTF-8">
+
+    <meta name = "viewport"
+        content="width=device-width, initial-scale=1.0">
+
+    <title>CJinversiones</title>
+
+</head>
+
+<body>
+
+    <h1>¡Bienvenido al Sistema Web Inmobiliario!</h1>
+
+</body>
+
+</html>
